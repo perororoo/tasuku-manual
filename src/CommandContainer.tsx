5927c67e22d788dc7e474f16aba5a63c8330ceab
peroroo <peroroo09@gmail.com>
2024-11-17 19:33:20 +0800

    Attempt to have a responsive UI in mobile

--- src/CommandContainer.tsx
+++ src/CommandContainer.tsx
@@ -16,7 +16,13 @@ function CommandContainer({ commandName, commandDesc, currentIndex, openedContai
           <path transform="matrix(.73391 .73391 -.73391 .73391 -1.2131 -10.584)" d="m27.346 12.629 3.8e-5 0.27929a5.0007 5.0007 44.996 0 0 5.0007 5h25.817a5 5 135 0 0 5-5v-25.817a5.0007 5.0007 45.004 0 0-5-5.0007l-0.27929-3.8e-5a4.9993 4.9993 135 0 0-5 4.9993v20.539h-20.539a4.9993 4.9993 135 0 0-4.9993 5z" fill="#4f4d46" strokeWidth=".79797"/>
         </svg>
       </div>
-      <h3 className="nunito-semibold default-font commandName">{ commandName }</h3><p className="nunito-normal default-font commandDesc">{"- " +  commandDesc }</p>
+        
+      <div className="commandNameContainer">
+        <h3 className="nunito-semibold default-font commandName">{ commandName }</h3>
+        <div className="commandDescContainer">
+          <p className="nunito-normal default-font commandDesc">{"- " +  commandDesc }</p>
+        </div>
+      </div>
     </div>
   );
 };
